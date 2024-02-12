f5510c85e5a06284b6a7d1eb1f7c83660cf4693d
lamnaourm <mohammed.lamnaour@gmail.com>
2024-02-12 17:27:37 +0100

    Correction

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import books from './books.js'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/catalogue', (req, res) => {
+    res.status(207).json(books);
+})
+
+app.get('/livre/:id', (req, res) => {
+    const id = req.params.id;
+    if(books.filter(e => e.id == id).length  == 0)
+        res.status(404).send('Id de livre Incorrect')
+    else 
+        res.status(201).json(livres)
+})
+
+app.listen(3000, (err) => {
+    if(!err)
+        console.log('Server started')
+    else 
+        console.log('Server not started')
+})
